91ed862492b037ca399e63be0e298210e8b6e9a9
Orihuela03 <Orihuelamarcos03@gmail.com>
2025-02-06 18:15:41 +0000

    Add getDishesByTable.

--- src/main/java/com/sergio/fastservice/controllers/DishesController.java
+++ src/main/java/com/sergio/fastservice/controllers/DishesController.java
@@ -1,6 +1,8 @@
 package com.sergio.fastservice.controllers;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.sergio.fastservice.entity.models.Tables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sergio.fastservice.entity.models.Dishes;
 import com.sergio.fastservice.entity.services.DishServicelmpl;
+import com.sergio.fastservice.entity.services.TableServiceImpl;
+
 
 
 @RestController
@@ -19,31 +23,41 @@ import com.sergio.fastservice.entity.services.DishServicelmpl;
 public class DishesController {
  @Autowired
  DishServicelmpl dishesService;
- 
- @GetMapping("/dishes")	
+
+ @Autowired
+ TableServiceImpl tableService;
+
+ @GetMapping("/dishes")
  public List<Dishes> getAllIngredients(){
 	 return dishesService.getAll();
  }
- 
+
  @GetMapping("/dishes/{id}")
  public Dishes getOne(@PathVariable(value="id")long id) {
 	 return dishesService.get(id);
  }
- 
 
- 
  @PostMapping("/dishes")
  public void post(Dishes dish) {
 	 dishesService.post(dish);
  }
- 
+
  @PutMapping("/dishes/{id}")
  public void put(Dishes dishes, @PathVariable(value = "id") long id) {
 	 dishesService.put(dishes, id);
  }
- 
+
  @DeleteMapping("/dishes/{id}")
  public void delete(@PathVariable(value = "id") long id) {
 	 dishesService.delete(id);
  }
+
+ @GetMapping("/tables/{id}/dishes")
+ public List<Dishes> getDishesByTable(@PathVariable(value = "id") long tableId) {
+     Tables table = tableService.get(tableId);
+     if (table != null) {
+         return table.getDishes();
+     }
+     return new ArrayList<>();
+    }
 }

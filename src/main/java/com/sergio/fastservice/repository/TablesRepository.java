8c0decb5552974d529a7a2a37ee5d2b930c2a8bc
Orihuela03 <Orihuelamarcos03@gmail.com>
2025-05-13 21:42:01 +0100

    Added the relationship between restaurants and tables

--- src/main/java/com/sergio/fastservice/repository/TablesRepository.java
+++ src/main/java/com/sergio/fastservice/repository/TablesRepository.java
@@ -4,6 +4,9 @@ import com.sergio.fastservice.entity.TablesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TablesRepository extends JpaRepository<TablesEntity, Long> {
+    List<TablesEntity> findByRestaurantId(Long restaurantId);
 }
